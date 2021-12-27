7a274b3e5bf116cdba5631b1f5dc698645770471
John Mickey <john.mickey@trility.io>
2021-12-27 12:18:49 -0600

    Tag testing

--- tests/integration/profile/controls/eks.rb
+++ tests/integration/profile/controls/eks.rb
@@ -2,6 +2,8 @@ control 'eks' do
   title 'eks'
   desc 'Testing EKS configuration'
   describe aws_eks_cluster(cluster_name: 'vault') do
-    it { should exist }
+    its('version') { should eq '1.21' }
+    its('tags') { should include(key: 'Environment', value: 'Test') }
+    its('tags_hash') { should include('tf_managed' => 'True') }
   end
 end
