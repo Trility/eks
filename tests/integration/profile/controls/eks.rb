fc38e7858290ba9c5f0a6e12f8e3fdc60c7d49ba
John Mickey <john.mickey@trility.io>
2022-01-04 08:28:51 -0600

    inspec

--- tests/integration/profile/controls/eks.rb
+++ tests/integration/profile/controls/eks.rb
@@ -1,10 +1,8 @@
 control 'eks' do
-  title 'eks'
-  desc 'EKS configuration'
+  title 'EKS Configuration'
   describe aws_eks_cluster('vault') do
     its('version') { should eq '1.21' }
     its('tags') { should include("Environment"=>"test") }
-    # its('tags') { should include(key: 'Environment', value: 'Test') }
     its('role_arn') { should cmp 'arn:aws:iam::533618305027:role/vault-eks-cluster' }
   end
 end
